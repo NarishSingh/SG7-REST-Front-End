c4f04a8c407f091d80e31528501e33326d7edd44
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-31 17:56:23 -0400

    code along notes and formatting

--- jQuery-HTMLElements-ElementsClassesStyles/js/home.js
+++ jQuery-HTMLElements-ElementsClassesStyles/js/home.js
@@ -9,7 +9,20 @@ $(document).ready(function () {
     $("#emptyDiv").append("p").text("A new paragraph of text...");
     $("#emptyDiv").prepend("p").text("yerrrr");
 
+    //css styling to all h1 elements
+    $('H1').css("color", "red");
+
     // set a CSS style on an element
+    // will override previous
     $("#first").css("color", "blue");
 
+    //css styling to container
+    $("#myDiv").append("p").text("Testing a cornflower blue styling");
+    $("#myDiv").css({
+        'width': '400',
+        'background-color': 'CornflowerBlue'
+    });
+
+    //
+    
 })
