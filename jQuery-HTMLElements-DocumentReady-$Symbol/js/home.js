e8149d0a0dfd132b3196d6f984fd9b83f0e70a52
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-31 14:27:05 -0400

    from code along

--- jQuery-HTMLElements-DocumentReady-$Symbol/js/home.js
+++ jQuery-HTMLElements-DocumentReady-$Symbol/js/home.js
@@ -1,3 +1,13 @@
+/*
+function handleReady(){
+    alert("Ready to go!")
+}
+
+// run a named function when doc is ready
+$(document).ready(handleReady);
+*/
+
+// run a anon func when doc is ready
 $(document).ready(function(){
     alert("Ready to go!!!");
 })
