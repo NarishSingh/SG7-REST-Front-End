e92af28faa6c49dfc44edda5d9384f5140f0e700
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-03 14:12:05 -0400

    Load contacts functionality complete

--- M3L6-ContactListREST/js/home.js
+++ M3L6-ContactListREST/js/home.js
@@ -1,4 +1,38 @@
+function loadContacts() {
+    var contentRows = $('contentRows');
+
+    $.ajax({
+        type: 'GET',
+        // url: 'http:/localhost:8080/contacts',
+        url: 'https://tsg-contactlist.herokuapp.com/contacts',
+        success: function (contactArray) {
+            alert("connected");
+
+            $.each(contactArray, function (index, contact) {
+                var name = contact.firstName + ' ' + contact.lastName;
+                var company = contact.company;
+
+                var row = '<tr>';
+                row += '<td> + name + ' < /td>';
+                row += '<td> + company + ' < /td>';
+                row += '<td><a>Edit</a></td>';
+                row += '<td><a>Delete</a></td>';
+                row += '</tr>';
+
+                contentRows.append(row);
+            });
+        },
+        error: function () {
+            $('#errorMessages')
+                .append($('<li>'))
+                .attr({class: 'list-group-item list-group-item-danger'})
+                .text('Error calling web service.');
+        }
+    });
+}
+
 $(document).ready(function () {
 
+    loadContacts();
 
 });
