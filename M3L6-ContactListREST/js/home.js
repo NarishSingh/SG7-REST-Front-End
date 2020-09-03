dda654603a7a53f90d931d385c55fe54d5b04d22
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-03 17:17:00 -0400

    updated to latest bootstrap 4 and jQuery libraries

--- M3L6-ContactListREST/js/home.js
+++ M3L6-ContactListREST/js/home.js
@@ -1,20 +1,23 @@
 function loadContacts() {
-    var contentRows = $('contentRows');
+    clearContactTable();
+
+    var contentRows = $('#contentRows');
 
     $.ajax({
         type: 'GET',
         // url: 'http:/localhost:8080/contacts',
         url: 'https://tsg-contactlist.herokuapp.com/contacts',
-        success: function (contactArray) {
+        success: function (data, status) {
             alert("connected");
 
-            $.each(contactArray, function (index, contact) {
+            $.each(data, function (index, contact) {
                 var name = contact.firstName + ' ' + contact.lastName;
                 var company = contact.company;
+                var id = contact.contactId;
 
                 var row = '<tr>';
-                row += '<td> + name + ' < /td>';
-                row += '<td> + company + ' < /td>';
+                row += '<td>' + name + '</td>';
+                row += '<td>' + company + '</td>';
                 row += '<td><a>Edit</a></td>';
                 row += '<td><a>Delete</a></td>';
                 row += '</tr>';
@@ -31,8 +34,51 @@ function loadContacts() {
     });
 }
 
+function clearContactTable() {
+    $('#contentRows').empty();
+}
+
 $(document).ready(function () {
 
     loadContacts();
+
+    $(document).on('clock', '#add-button', function (event) {
+        $.ajax({
+            type: 'POST',
+            url: 'https://tsg-contactlist.herokuapp.com/contact',
+            data: JSON.stringify({
+                firstName: $('#add-first-name').val(),
+                lastName: $('#add-last-name').val(),
+                company: $('#add-company').val(),
+                phone: $('#add-phone').val(),
+                email: $('#add-email').val()
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            'dataType': 'json',
+            success: function (data, status) {
+                //clear error msgs
+                $('#errorMessages').empty();
+
+                //Clear form
+                $('#add-first-name').val('');
+                $('#add-last-name').val('');
+                $('#add-company').val('');
+                $('#add-phone').val('');
+                $('#add-email').val('');
+
+                //its been POSTed, reload to see new contact
+                loadContacts();
+            },
+            error: function () {
+                $('#errorMessages')
+                    .append($('<li>'))
+                    .attr({class: 'list-group-item list-group-item-danger'})
+                    .text('Error calling web service.');
+            }
+        })
+    });
 
 });
