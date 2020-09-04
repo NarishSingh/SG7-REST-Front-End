cceb4a8e7312b854ff9d2c688a1e8157fd32f582
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-04 14:56:12 -0400

    Starting open weather exercise

---
+++ Ex1-OpenWeather/js/home.js
@@ -0,0 +1,10 @@
+/*
+server name: api.openweathermap.org
+example url forecast: http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=f29ed23e97e67d948334f9b71c66421d
+docs/examples: https://openweathermap.org/current
+ */
+
+$(document).ready(function () {
+
+})
+
