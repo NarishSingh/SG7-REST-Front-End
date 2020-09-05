dce5767e35ea015085bf3d2ab84f0c8d3b48b7a3
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-05 18:47:03 -0400

    fixes to unit selection

--- Ex1-OpenWeather/js/home.js
+++ Ex1-OpenWeather/js/home.js
@@ -12,7 +12,13 @@ $(document).ready(function () {
         $('#conditions-row').show();
         $('#forecast-row').show();
 
-        getConditionsWeather();
+        //retrieve zipcode
+        var zip = $('#add-zip-code').val().text();
+
+        //retrieve unit type
+        var unit = $('#select-units-dropdown').val($(this)).text();
+
+        getConditionsWeather(zip, unit);
     });
 })
 
@@ -22,44 +28,31 @@ $(document).ready(function () {
 /**
  * Retrieve and render conditions and weather data for a given zip code on click
  */
-function getConditionsWeather() {
-    //retrieve zipcode
-    var zip = $('#add-zip-code').val().text();
+function getConditionsWeather(zipcode, unitType) {
 
-    //retrieve unit type
-    var unitType = $('#select-units-dropdown').val($(this)).text();
 
-    //ajax calls by unit system
-    if (unitType === "Imperial") {
-        $.ajax({
-            type: 'GET',
-            url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + zip + ',us&units=imperial&APPID=f29ed23e97e67d948334f9b71c66421d',
-            success: function (data, status) {
-                //TODO implement
-                //append city name to #city-header
-            },
-            error: function () {
-                $('#errorMessages')
-                    .append($('<li>'))
-                    .attr({class: 'list-group-item list-group-item-danger'})
-                    .text('Error calling web service.');
-            }
-        });
-    } else { //metric
-        $.ajax({
-            type: 'GET',
-            url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + zip + ',us&units=metric&APPID=f29ed23e97e67d948334f9b71c66421d',
-            success: function (data, status) {
-                //TODO implement
-                //append city name to #city-header
-            },
-            error: function () {
-                $('#errorMessages')
-                    .append($('<li>'))
-                    .attr({class: 'list-group-item list-group-item-danger'})
-                    .text('Error calling web service.');
-            }
-        });
-    }
+    //format url with zipcode and insert unit system
+    var urlForApi = 'http://api.openweathermap.org/data/2.5/forecast?q=' + zipcode + ',us&units=';
+    urlForApi += unitType + '&APPID=f29ed23e97e67d948334f9b71c66421d';
 
+    $.ajax({
+        type: 'GET',
+        url: urlForApi,
+        success: function (weatherData) {
+            alert("Retrieved");
+
+            //TODO implement
+
+            //append city name to #city-header
+            var cityHeaderText = "Current Conditions in " + weatherData.city.name;
+            $('#city-header').append('<p>').text(cityHeaderText);
+
+        },
+        error: function () {
+            $('#errorMessages')
+                .append($('<li>'))
+                .attr({class: 'list-group-item list-group-item-danger'})
+                .text('Error calling web service.');
+        }
+    });
 }
