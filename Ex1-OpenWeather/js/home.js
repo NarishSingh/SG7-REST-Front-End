d3bf342ed620544ad3185f46c499ec784bd7ed3f
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-05 17:51:04 -0400

    js functions drafted, ajax requests set up

--- Ex1-OpenWeather/js/home.js
+++ Ex1-OpenWeather/js/home.js
@@ -7,34 +7,59 @@ docs/examples: https://openweathermap.org/current
 // MAIN
 //////////////////////////////////////////////////////////
 $(document).ready(function () {
-    //unit handler
-    var unitType = $('#select-units-dropdown').val($(this)).text();
-
     //Get Weather btn handler
-    $(document).on("click", "#get-weather-btn", getConditionsWeather(unitType));
-})
+    $(document).on('click', '#get-weather-btn', function (event) {
+        $('#conditions-row').show();
+        $('#forecast-row').show();
 
+        getConditionsWeather();
+    });
+})
 
 //////////////////////////////////////////////////////////
 // METHODS
 //////////////////////////////////////////////////////////
 /**
  * Retrieve and render conditions and weather data for a given zip code on click
- * @param unitType {String} unit system to use, either Imperial or Metric
  */
-function getConditionsWeather(unitType) {
-    $('#conditions-row').show();
-    $('#forecast-row').show();
-
+function getConditionsWeather() {
     //retrieve zipcode
+    var zip = $('#add-zip-code').val().text();
 
-    //append city name to #city-header
+    //retrieve unit type
+    var unitType = $('#select-units-dropdown').val($(this)).text();
 
     //ajax calls by unit system
     if (unitType === "Imperial") {
-
+        $.ajax({
+            type: 'GET',
+            url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + zip + ',us&units=imperial&APPID=f29ed23e97e67d948334f9b71c66421d',
+            success: function (data, status) {
+                //TODO implement
+                //append city name to #city-header
+            },
+            error: function () {
+                $('#errorMessages')
+                    .append($('<li>'))
+                    .attr({class: 'list-group-item list-group-item-danger'})
+                    .text('Error calling web service.');
+            }
+        });
     } else { //metric
-
+        $.ajax({
+            type: 'GET',
+            url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + zip + ',us&units=metric&APPID=f29ed23e97e67d948334f9b71c66421d',
+            success: function (data, status) {
+                //TODO implement
+                //append city name to #city-header
+            },
+            error: function () {
+                $('#errorMessages')
+                    .append($('<li>'))
+                    .attr({class: 'list-group-item list-group-item-danger'})
+                    .text('Error calling web service.');
+            }
+        });
     }
-}
 
+}
