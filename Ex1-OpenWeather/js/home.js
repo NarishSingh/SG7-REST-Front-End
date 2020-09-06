dc77e4f9fbc99506ce2c2bd3c8156add5c073d45
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-06 17:17:50 -0400

    Created 2 API calls, one for today, one for 5 day forecast

--- Ex1-OpenWeather/js/home.js
+++ Ex1-OpenWeather/js/home.js
@@ -29,21 +29,61 @@ function getConditionsWeather() {
     //retrieve unit type
     var unit = $('#select-units');
 
-    //format url with zipcode and insert unit system
-    var urlForApi = 'http://api.openweathermap.org/data/2.5/forecast?q=' + zip + ',us&units=';
-    urlForApi += unit + '&APPID=f29ed23e97e67d948334f9b71c66421d';
+    //CURRENT URL
+    var urlForToday = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&APPID=f29ed23e97e67d948334f9b71c66421d';
 
+    //FORECAST URL - format url with zipcode and insert unit system
+    var urlForApi = 'http://api.openweathermap.org/data/2.5/forecast?q=' + zip + ',us&units=' + unit + '&APPID=f29ed23e97e67d948334f9b71c66421d';
+
+    /*CURRENT WEATHER*/
+    $.ajax({
+        type: 'GET',
+        url: urlForToday,
+        success: function (weatherData, status) {
+            /*header*/
+            //append city name to #city-header
+            var cityHeaderText = "Current Conditions in " + weatherData.name;
+            $('#city-header').append('<h1></h1>').text(cityHeaderText);
+
+            /*current conditions*/
+            //icon
+            /*
+            $.ajax({
+                type: 'GET',
+                url: 'http://openweathermap.org/img/w/' + iconId + '.png',
+                success: function (iconPic, status) {
+                    alert("Retrieved icon");
+
+                    //TODO append pic, do css to center it
+                },
+                error: function () {
+                    $('#errorMessages')
+                        .append($('<li>'))
+                        .attr({class: 'list-group-item list-group-item-danger'})
+                        .text('Error calling web service.');
+                }
+            });
+             */
+
+            //description
+
+
+        },
+        error: function () {
+            $('#errorMessages')
+                .append($('<li>'))
+                .attr({class: 'list-group-item list-group-item-danger'})
+                .text('Error calling web service.');
+        }
+    });
+
+    /*5 DAY FORECAST*/
     $.ajax({
         type: 'GET',
         url: urlForApi,
         success: function (weatherData, status) {
-            //TODO implement
+            var forecastArray = weatherData.list; //each day in an array [0-4]
 
-            //append city name to #city-header
-            var cityHeaderText = "Current Conditions in " + weatherData.city.name;
-            $('#city-header').append('<h1></h1>').text(cityHeaderText);
-
-            //icon
 
         },
         error: function () {
