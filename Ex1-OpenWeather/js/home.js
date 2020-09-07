a8972acf91ba8cb6cd02da9e6f110bd7d02b928d
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-07 13:29:49 -0400

    Current Conditions row fully implemented, fixed issue with icon not being replace

--- Ex1-OpenWeather/js/home.js
+++ Ex1-OpenWeather/js/home.js
@@ -9,6 +9,7 @@ docs/examples: https://openweathermap.org/current
 $(document).ready(function () {
     //Get Weather btn handler
     $(document).on('click', '#get-weather-btn', function (event) {
+
         $('#conditions-row').show();
         $('#forecast-row').show();
 
@@ -28,10 +29,10 @@ function getConditionsWeather() {
     var zip = $('#add-zip-code').val();
 
     //retrieve unit type
-    var unit = $('#select-units');
+    var unit = $('#select-units').val();
 
     //CURRENT URL
-    var urlForToday = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&APPID=f29ed23e97e67d948334f9b71c66421d';
+    var urlForToday = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&units=' + unit + '&APPID=f29ed23e97e67d948334f9b71c66421d';
 
     //FORECAST URL - format url with zipcode and insert unit system
     var urlForApi = 'http://api.openweathermap.org/data/2.5/forecast?q=' + zip + ',us&units=' + unit + '&APPID=f29ed23e97e67d948334f9b71c66421d';
@@ -51,14 +52,32 @@ function getConditionsWeather() {
             var iconId = weatherData.weather[0].icon;
             var iconSRC = 'http://openweathermap.org/img/w/' + iconId + '.png'
 
-            $('#condition-icon').append('<img src="' + iconSRC + '" alt="Current conditions icon">');
+            $('#condition-icon').replaceWith('<img src="' + iconSRC + '" class="img-fluid" alt="Current conditions icon">');
 
             //description
             var descriptionString = weatherData.weather[0].main + ": " + weatherData.weather[0].description;
-            $('#condition-desc').append('<p></p>').text(descriptionString);
+            $('#condition-desc').append('<p class="text-center"></p>').text(descriptionString);
 
             /*stats*/
+            var tempString = "Temperature: " + weatherData.main.temp;
+            if (unit === "imperial") {
+                tempString += "°F";
+            } else {
+                tempString += "°C";
+            }
 
+            var humidString = "Humidity: " + weatherData.main.humidity + "%";
+
+            var windString = "Wind: " + weatherData.wind.speed;
+            if (unit === "imperial") {
+                windString += " mph";
+            } else {
+                windString += " km/h";
+            }
+
+            $('#temp').append('<p></p><br>').text(tempString);
+            $('#humidity').append('<p></p><br>').text(humidString);
+            $('#wind').append('<p></p><br>').text(windString);
         },
         error: function () {
             $('#errorMessages')
