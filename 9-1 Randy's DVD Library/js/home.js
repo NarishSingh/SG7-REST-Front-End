143a1723505f0b490f8b40a2ce4312fd926bcf17
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-10 13:08:57 -0400

    Adding lecture files, working on DVD library

--- 9-1 Randy's DVD Library/js/home.js
+++ 9-1 Randy's DVD Library/js/home.js
@@ -1,4 +1,5 @@
 let ds = new DataService();
+
 // ///////////////////////////////////////////////////////////
 // Page manipulation methods
 function formatRow(dvd) {
@@ -8,52 +9,64 @@ function formatRow(dvd) {
     <td>${dvd.director}</td>
     <td>${dvd.rating}</td>
     <td>
-        <a href="#" data-dvdid='${dvd.dvdId}' class='editDvd'><i class="fa fa-pencil"></i></a>
-        <a href="#" data-dvdid='${dvd.dvdId}' class='deleteDvd'><i class="fa fa-trash"></i></a>
+        <a href="#" data-dvdid='${dvd.id}' class='editDvd'><i class="fa fa-pencil"></i></a>
+        <a href="#" data-dvdid='${dvd.id}' class='deleteDvd'><i class="fa fa-trash"></i></a>
     </td>
 </tr>`
 }
 
+
 function refreshTable(dvds) {
     let dvdTable = $("#dvdTable>tbody");
     dvdTable.empty();
+
     for (let i = 0; i < dvds.length; i++) {
         const dvd = dvds[i];
         $(dvdTable).append(formatRow(dvd));
     }
 }
+
+
 // /////////////////////////////////////////////////////////////////////
 // Page event methods
 function onAddDvdSubmit(e) {
     e.preventDefault();
+
     let form = $(this);
-    let dvd = {
-        dvdId: 0,
+
+    let dvd =
+    {
+        id: 0,
         title: $("#title").val(),
         releaseYear: $("#releaseYear").val(),
         director: $("#director").val(),
         rating: $("#rating").val()
     };
-    ds.addDvd(dvd, function() {
+
+    ds.addDvd(dvd, function () {
         ds.getDvds(refreshTable, alertError);
     }, alertError);
+
     $(form)[0].reset();
 }
 
 function onDeleteDvdClicked(e) {
     e.preventDefault();
+
     var dvdId = $(this).data('dvdid');
-    ds.removeDvd(dvdId, function() {
+    ds.removeDvd(dvdId, function(){
         ds.getDvds(refreshTable, alertError);
     }, alertError);
+    
 }
 
 function onEditDvdClicked(e) {
     e.preventDefault();
+
     var dvdId = $(this).data("dvdid");
-    ds.getDvdById(dvdId, function(dvd) {
+    ds.getDvdById(dvdId, function(dvd){
         if (dvd) {
-            $("#editDvdId").val(dvd.dvdId);
+            $("#editDvdId").val(dvd.id);
             $("#editTitle").val(dvd.title);
             $("#editReleaseYear").val(dvd.releaseYear);
             $("#editDirector").val(dvd.director);
@@ -61,33 +74,43 @@ function onEditDvdClicked(e) {
             $("#editDvdModal").modal('show');
         }
     }, alertError);
+
+    
 }
 
 function onEditDvdSubmit(e) {
     e.preventDefault();
-    let dvd = {
-        dvdId: $("#editDvdId").val(),
+
+    let dvd =
+    {
+        id: $("#editDvdId").val(),
         title: $("#editTitle").val(),
         releaseYear: $("#editReleaseYear").val(),
         director: $("#editDirector").val(),
         rating: $("#editRating").val()
     };
-    setTimeout(function() {
-        ds.updateDvd(dvd, function() {
-            ds.getDvds(refreshTable, alertError);
-        }, alertError);
-        $("#editDvdModal").modal('hide');
-    }, 1000);
+
+    ds.updateDvd(dvd, function(){
+        ds.getDvds(refreshTable, alertError);
+    }, alertError);
+    
+    $("#editDvdModal").modal('hide');
+    
 }
 
 function alertError(msg) {
-    alert(msg);
+    alert(msg.responseJSON.message);
 }
+
+
 // ////////////////////////////////////////////////////////////////
-$(document).ready(function() {
+$(document).ready(function () {
     ds.getDvds(refreshTable, alertError);
+
     $(document).on('submit', '#addDvd', onAddDvdSubmit);
     $(document).on('submit', '#editDvd', onEditDvdSubmit);
+
     $(document).on('click', '.deleteDvd', onDeleteDvdClicked);
+
     $(document).on('click', '.editDvd', onEditDvdClicked);
 });
--- 9-1 Randy's DVD Library/js/mockDataService.js
+++ 9-1 Randy's DVD Library/js/mockDataService.js
@@ -1,56 +1,72 @@
-var DataService = function() {
+var DataService = function () {
     var self = this;
+
     let counter = 2;
-    let dvdList = [{
-        dvdId: 1,
-        title: 'Crossroads',
-        releaseYear: '1986',
-        director: 'Walter Hill',
-        rating: 'R'
-    }];
+
+    let dvdList = [
+        {
+            dvdId: 1,
+            title: 'Crossroads',
+            releaseYear: '1986',
+            director: 'Walter Hill',
+            rating: 'R'
+        }
+    ];
+
     // //////////////////////////////////////////////////////////////////
     // Data manipulation methods
     self.getDvds = function(callback, error) {
-        setTimeout(function() {
+        setTimeout(function(){
             callback(dvdList);
         }, 500);
     }
-    self.addDvd = function(dvd, callback, error) {
-        setTimeout(function() {
+
+    self.addDvd = function(dvd, callback, error){
+        setTimeout(function(){
             dvd.dvdId = counter++;
             dvdList.push(dvd);
             callback();
         }, 500);
+
         //error();
     }
-    self.removeDvd = function(dvdId, callback, error) {
+
+    self.removeDvd = function (dvdId, callback, error) {
         let tempList = [];
+
         for (let i = 0; i < dvdList.length; i++) {
             const dvd = dvdList[i];
             if (dvd.dvdId != dvdId) {
                 tempList.push(dvd);
             }
         }
+
         dvdList = tempList;
-        setTimeout(function() {
+
+        setTimeout(function(){
             callback();
         }, 500);
+        
         //error();
     }
-    self.getDvdById = function(dvdId, callback, error) {
+
+    self.getDvdById = function (dvdId, callback, error) {
         setTimeout(function() {
             for (let i = 0; i < dvdList.length; i++) {
                 const dvd = dvdList[i];
                 if (dvd.dvdId == dvdId) {
-                    callback(dvd);
-                    return;
+                     callback(dvd);
+                     return;
                 }
             }
+    
             error("Dvd was not found");
         }, 500);
+        
     }
-    self.updateDvd = function(dvd, callback, error) {
-        setTimeout(function() {
+
+    self.updateDvd = function (dvd, callback, error) {
+        setTimeout(function(){
             for (let i = 0; i < dvdList.length; i++) {
                 if (dvd.dvdId == dvdList[i].dvdId) {
                     dvdList[i] = dvd;
@@ -58,7 +74,10 @@ var DataService = function() {
                     return;
                 }
             }
+
             error("Dvd was not found.");
         }, 500);
+        
     }
+
 }
