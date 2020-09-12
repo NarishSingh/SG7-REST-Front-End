81f04f2e4487bf6cd4ac5a835953726baf2c2150
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-12 15:38:32 -0400

    slight changes to html, load from server implemented

--- Ex2-DVDLibAPI/js/home.js
+++ Ex2-DVDLibAPI/js/home.js
@@ -1,7 +1,9 @@
 ////////////////////////////////////////////////////////
 // MAIN
 ////////////////////////////////////////////////////////
-$(document).ready(function (){
+$(document).ready(function () {
+    loadLibrary();
+
     //create DVD button handler
     $(document).on('click', '#create-dvd-btn', function () {
 
@@ -16,9 +18,48 @@ $(document).ready(function (){
 ////////////////////////////////////////////////////////
 // Methods
 ////////////////////////////////////////////////////////
+/**
+ * Load all titles from server via GET and render to table
+ */
 function loadLibrary() {
     $.ajax({
-       type: 'GET',
-       url: '',
+        type: 'GET',
+        url: 'http://localhost:8080/dvds',
+        success: function (dvdArr, status) {
+            $.each(dvdArr, function (i, dvd) {
+                //extract fields, no need to display id nor notes
+                let title = dvd.title;
+                let releaseYr = dvd.releaseYear;
+                let director = dvd.director;
+                let rating = dvd.rating;
+
+                //render to tr
+                let dvdRow = '<tr>';
+                dvdRow += '<td>' + title + '</td>';
+                dvdRow += '<td>' + releaseYr + '</td>';
+                dvdRow += '<td>' + director + '</td>';
+                dvdRow += '<td>' + rating + '</td>';
+                dvdRow += '<td onclick="onEditDVD()"><i class="far fa-edit"></i></td>'; //edit
+                dvdRow += '<td onclick="onDeleteDVD()"><i class="far fa-trash-alt"></i></td>'; //delete
+
+                $('#library-entries').append(dvdRow);
+            });
+        },
+        error: function () {
+            $('#errorMessages')
+                .append($('<li>'))
+                .attr({class: 'list-group-item list-group-item-danger'})
+                .text('Error calling web service.');
+        }
     });
+}
+
+//TODO implement this for tr's -> edit and delete
+function onEditDVD() {
+
+
+}
+
+function onDeleteDVD() {
+
 }
